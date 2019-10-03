4c864f7e05f027886d679216ed508ab0b8715046
David Huang <bugman195@gmail.com>
2019-10-03 23:37:23 +0800

    Add Longest Common String (LCS)

---
+++ Algorithm/LCS/index.js
@@ -0,0 +1,26 @@
+function LCS(S1, m, S2, n) {
+    var finalResult;
+
+    //define base case. if the length of either strings are zero, 
+    //then no need to continue return 0
+    if (m === 0 || n === 0) {
+        finalResult = 0;
+    } else if (S1[m - 1] === S2[n - 1]) { //if the last characters are the same
+        finalResult = 1 + LCS(S1, m - 1, S2, n - 1);
+    } else {
+        var excludeLastOfS1 = LCS(S1, m - 1, S2, n),
+            excludeLastOfS2 = LCS(S1, m, S2, n - 1);
+
+        finalResult = Math.max(excludeLastOfS1, excludeLastOfS2);
+    }
+    //return the final result
+    return finalResult;
+}
+
+var string_one = "ABACBDAB", //BATD
+    string_one_length = string_one.length,
+    string_two = "BDCADB", //ABACD
+    string_two_length = string_two.length,
+    lcs = LCS(string_one, string_one_length, string_two, string_two_length);
+
+console.log("Longest Comment Subsequence is: ", lcs);
