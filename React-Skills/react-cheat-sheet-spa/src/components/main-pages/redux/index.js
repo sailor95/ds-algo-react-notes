05c4d0f802f9ea6051d1ec4c4a4fb7f77fbf859d
David Huang <bugman195@gmail.com>
2019-10-13 17:52:41 +0800

    [Redux] Add route & packages for boilerplate

--- React-Skills/react-cheat-sheet-spa/src/components/main-pages/redux/index.js
+++ React-Skills/react-cheat-sheet-spa/src/components/main-pages/redux/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 
+import ReduxBoilerplate from './boilerplate';
 import ReduxForm from './form/ReduxForm';
 
 const ReduxPage = () => {
-  return (
-    <>
-      <Route path="/redux/redux-form" component={ReduxForm} />
-    </>
-  );
+    return (
+        <>
+            <Route path="/redux/boilerplate" component={ReduxBoilerplate} />
+            <Route path="/redux/redux-form" component={ReduxForm} />
+        </>
+    )
 }
 
 export default ReduxPage;
