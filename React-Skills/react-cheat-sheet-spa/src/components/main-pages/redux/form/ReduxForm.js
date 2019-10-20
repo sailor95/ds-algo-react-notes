6fc61cd54846bf2e4ae12ed273e9cbe8951edd02
David Huang <bugman195@gmail.com>
2019-10-20 17:48:21 +0800

    Add Redux Form Flow pic

--- React-Skills/react-cheat-sheet-spa/src/components/main-pages/redux/form/ReduxForm.js
+++ React-Skills/react-cheat-sheet-spa/src/components/main-pages/redux/form/ReduxForm.js
@@ -3,15 +3,19 @@ import { Container, Row, Col } from 'react-bootstrap';
 
 import BaseForm from './BaseForm';
 import FormContainer from './FormContainer';
+import reduxFormFlowPic from '../../../../res/reduxFormDiagram.png'
 
 class ReduxForm extends React.Component {
   render() {
     return (
       <Container>
         <Row>
-          <Col>
+          <Col sm={12}>
             <h1>Redux Form</h1>
           </Col>
+          <Col sm={12} className="mt-4 mb-4 p-0">
+            <img src={reduxFormFlowPic} alt="Redux Form Flow" width="450px" />
+          </Col >
           <Col sm={12}>
             <h3>Base Form</h3>
           </Col>
